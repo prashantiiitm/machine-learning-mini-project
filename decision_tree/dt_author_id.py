e20ced229484874c6a1bdfca257b724d6deac026
Prashant Pandey <94prashant.pandey@gmail.com>
2016-03-12 13:16:53 +0530

    Completed the Decision tree assignment and designed Decision tree classifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,7 +19,18 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+clf=tree.DecisionTreeClassifier(min_samples_split=40);
+clf.fit(features_train,labels_train)
+test1=clf.predict(features_test);
+#print len(features_train[0])
+score=accuracy_score(test1,labels_test);
+print score
 
 
 #########################################################
