0e7783bb6340c7f4cd1829f800aaebd12049665c
Prashant Pandey <94prashant.pandey@gmail.com>
2016-03-24 18:49:02 +0530

    Modified the Text learning assignment and added the TF IDF matrix .

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,46 +41,54 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
-            
+        #temp_counter += 1
+        #if temp_counter < 200:
+        path = os.path.join('..', path[:-2]+"_")
+        #print path
+        email = open(path, "r")
+        
 
-            ### use parseOutText to extract the text from the opened email
-            content = parseOutText(email)
-            
-            
-            
-            
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-            stop_words = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf", "houectect"]
+        ### use parseOutText to extract the text from the opened email
+        tap = parseOutText(email)
+        
+        
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        stop_words = ["sara", "shackleton", "chris", "germani"]
 
-            for w in stop_words:
-                tap = tap.replace(w, "")
+        for w in stop_words:
+            tap = tap.replace(w, "")
 
-            ### append the text to word_data
-            word_data.append( tap )
+        ### append the text to word_data
+        word_data.append( tap )
 
-            if name == "sara":
-                from_data.append(0)
-            else:
-                from_data.append(1)
+        if name == "sara":
+            from_data.append(0)
+        else:
+            from_data.append(1)
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-            email.close()
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
+#print word_data[152]
+#print from_data
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
+from nltk.corpus import stopwords;
+stop = stopwords.words('english');
+#print stop;
+from sklearn.feature_extraction.text import TfidfVectorizer;
+tfidf=TfidfVectorizer(stop_words='english');
+ans=tfidf.fit_transform(word_data);
+git=tfidf.get_feature_names();
+#print len(git);
+print git[34597]
 
 
 ### in Part 4, do TfIdf vectorization here
